edb895442b6f10f5689c66fe64593a68103b8764
DishaLogs <ldisha2000@gmail.com>
2019-08-06 19:42:22 +0530

    update

--- app/src/main/java/com/example/myapplication/FourthActivity.java
+++ app/src/main/java/com/example/myapplication/FourthActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -43,6 +44,8 @@ public class FourthActivity extends AppCompatActivity implements OnClickListener
             msg=bundle.getString("message");
         set();
         //updateTextView();
+
+
     }
 
     @Override
